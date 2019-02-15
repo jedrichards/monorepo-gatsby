a217f0a5acab5fc56cba3fecee857b4a05bf99bf
josefaidt <josef.aidt@gmail.com>
2019-02-15 14:08:30 -0600

    initial commit

---
+++ packages/components/src/header/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const RedDiv = styled.div`
+  background-color: red;
+  color: white;
+`
+
+export default () => <RedDiv><h1>TESTING FROM COMPONENT LIBRARY</h1></RedDiv>
